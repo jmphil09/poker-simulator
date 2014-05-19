f40385b13a1680d70c25fd25770725ae75263d02
James Phillips <jmphil09@gmail.com>
2014-05-18 17:22:03 -0700

    finished converting from junit to scalatest

--- src/test/scala/handManagerSuite/HandManagerTest.scala
+++ src/test/scala/handManagerSuite/HandManagerTest.scala
@@ -49,190 +49,158 @@ class HandManagerTest extends FlatSpec with Matchers {
     countCopies(testlist4, testlist4, 0) should be (2)
   }
   
-  "convertHand" should "work properly" in {
+  "handAsOrderedPair" should "create a set and list for a hand" in {
     val hand1 = "3H 3H 4C 5S 8C"
-      convertHand(hand1) should be ((Set(3,4,5,8),List(3,3,4,5,8)))
+      handAsOrderedPair(hand1) should be ((Set(3,4,5,8),List(3,3,4,5,8)))
   }
   
-  /*
-  
-  //tests for highcard
-  @Test
-  def highCardTiebreaker() = {
+  "highCardTiebreaker" should "break ties between 2 high card hands" in {
     val highCardTest1 = "2H 3H 4C 5S 8C 2H 3D 4C 5S 7S"
     val highCardTest2 = "AH KH QC JS 3C AH KD QC JS 2S"
     val highCardTest3 = "AH KH QC JS 2C KH QD TC 5S 7S"
-    assert(convertHand("AH KH QC JS 2C") === (Set(14, 13, 2, 12, 11), List(14, 13, 12, 11, 2)))
+    handAsOrderedPair("AH KH QC JS 2C") should be ((Set(14, 13, 2, 12, 11), List(14, 13, 12, 11, 2)))
 
-    assert(findWinner(highCardTest1) === "Player 1")
-    assert(findWinner(highCardTest2) === "Player 1")
-    assert(findWinner(highCardTest3) === "Player 1")
+    findWinner(highCardTest1) should be ("Player 1")
+    findWinner(highCardTest2) should be ("Player 1")
+    findWinner(highCardTest3) should be ("Player 1")
   }
 
-  //tests for onepair
-  @Test
-  def onePair() {
+  "onePair hands" should "work properly" in {
     val onePairTest1 = "3H 3S 4D 2S 6D 9S TD 2C 3C 7S"
-    assert(findHand("3H 3S 4D 2S 6D") === OnePair("3H 3S 4D 2S 6D"))
-    assert(findWinner(onePairTest1) === "Player 1")
+    findHand("3H 3S 4D 2S 6D") should be (OnePair("3H 3S 4D 2S 6D"))
+    findWinner(onePairTest1) should be ("Player 1")
   }
 
-  @Test
-  def onePairTiebreaker() {
+  "onePairTiebreaker" should "break ties between 2 one pair hands" in {
     val onePairTest1 = "3H 3H 4C 5S 8C 2H 2D 4C 5S 7S"
     val onePairTest2 = "2H 2H 4C 5S AC 2H 2D 4C 5S 7S"
     val onePairTest3 = "3H 3H 8C 7S 6C 3H 3D 8C 5S 7S"
-    assert(findWinner(onePairTest1) === "Player 1")
-    assert(findWinner(onePairTest2) === "Player 1")
-    assert(findWinner(onePairTest3) === "Player 1")
+    findWinner(onePairTest1) should be ("Player 1")
+    findWinner(onePairTest2) should be ("Player 1")
+    findWinner(onePairTest3) should be ("Player 1")
     val onePairTest4 = "9H 4D JC KS JS TH 8H 5C QS TC"
-    assert(findHand("9H 4D JC KS JS") === OnePair("9H 4D JC KS JS"))
-    assert(findHand("TH 8H 5C QS TC") === OnePair("TH 8H 5C QS TC"))
-    assert(findWinner(onePairTest4) === "Player 1")
+    findHand("9H 4D JC KS JS") should be (OnePair("9H 4D JC KS JS"))
+    findHand("TH 8H 5C QS TC") should be (OnePair("TH 8H 5C QS TC"))
+    findWinner(onePairTest4) should be ("Player 1")
   }
 
-  @Test
-  def determinePairTest() {
+  "determinePair" should "find a single pair in a list" in {
     val list1 = List(9, 4, 11, 13, 11)
     val list2 = List(10, 8, 5, 12, 10)
-    assert(determinePair(list1) === 11)
-    assert(determinePair(list2) === 10)
+    determinePair(list1) should be (11)
+    determinePair(list2) should be (10)
   }
 
-  //tests for twopair
-  @Test
-  def twoPair() {
+  "twoPair hands" should "work properly" in {
     val twoPairTest1 = "3H 3S 2D 2S 6D 9S 9D 2C 3C 7S"
-    assert(findHand("3H 3S 2D 2S 6D") === TwoPair("3H 3S 2D 2S 6D"))
-    assert(findWinner(twoPairTest1) === "Player 1")
+    findHand("3H 3S 2D 2S 6D") should be (TwoPair("3H 3S 2D 2S 6D"))
+    findWinner(twoPairTest1) should be ("Player 1")
   }
 
-  @Test
-  def twoPairTiebreaker() {
+  "twoPairTiebreaker" should "break ties between 2 two pair hands" in {
     val twoPairTest1 = "3H 3D 4C 4S 8C 3H 3D 4C 4S 7S"
-    assert(findWinner(twoPairTest1) === "Player 1")
+    findWinner(twoPairTest1) should be ("Player 1")
   }
 
-  //tests for 3 of a kind
-  @Test
-  def threeOfAKind() {
+  "threeOfAKind hands" should "work properly" in {
     val threeOfAKindTest1 = "3H 3S 3D 5S 6D 9S 9D 2C 3C 7S"
-    assert(findHand("3H 3S 3D 5S 6D") === ThreeOfAKind("3H 3S 3D 5S 6D"))
-    assert(findWinner(threeOfAKindTest1) === "Player 1")
+    findHand("3H 3S 3D 5S 6D") should be (ThreeOfAKind("3H 3S 3D 5S 6D"))
+    findWinner(threeOfAKindTest1) should be ("Player 1")
   }
 
-  @Test
-  def threeOfAKindTiebreaker() {
+  "threeOfAKindTiebreaker" should "break ties between 2 3 of a kind hands" in {
     val threeOfAKindTest1 = "9S 9D 9C 3C 7S 3H 3S 3D 5S 6D"
-    assert(findHand("9S 9D 9C 3C 7S") === ThreeOfAKind("9S 9D 9C 3C 7S"))
-    assert(findHand("3H 3S 3D 5S 6D") === ThreeOfAKind("3H 3S 3D 5S 6D"))
-    assert(findWinner(threeOfAKindTest1) === "Player 1")
+    findHand("9S 9D 9C 3C 7S") should be (ThreeOfAKind("9S 9D 9C 3C 7S"))
+    findHand("3H 3S 3D 5S 6D") should be (ThreeOfAKind("3H 3S 3D 5S 6D"))
+    findWinner(threeOfAKindTest1) should be ("Player 1")
   }
 
-  //tests for a straight
-  @Test
-  def straight() {
+  "straight hands" should "work properly" in {
     val straightTest1 = "3H 2H 4H 5S 6D 9S 9D 2C 3S 7S"
-    assert(findHand("3H 2H 4H 5S 6D") === Straight("3H 2H 4H 5S 6D"))
-    assert(findWinner(straightTest1) === "Player 1")
+    findHand("3H 2H 4H 5S 6D") should be (Straight("3H 2H 4H 5S 6D"))
+    findWinner(straightTest1) should be ("Player 1")
     val straightTest2 = "3H 2H 4H 5S AD 9S 9D 2C 3S 7S" 
-    assert(findHand("3H 2H 4H 5S AD")===Straight("3H 2H 4H 5S AD"))
+    findHand("3H 2H 4H 5S AD")should be (Straight("3H 2H 4H 5S AD"))
   }
   
-  //tests for wheel straight
-  @Test
-  def wheelTest() {
+  it should "also work with wheel straights" in {
     //test highest vs lowest vs wheel
     val wheelHand = "AC 2D 3H 4S 5S"
     val lowStraight = "2H 3S 4C 5H 6H"
     val highStraight = "AD KH QH JH TH"
-    assert(findHand(wheelHand)===Straight(wheelHand))
-    assert(findWinner(wheelHand + " " + lowStraight)==="Player 2")
-    assert(findWinner(wheelHand + " " + highStraight)==="Player 2")
-    assert(findWinner(highStraight + " " + lowStraight)==="Player 1")
+    findHand(wheelHand) should be (Straight(wheelHand))
+    findWinner(wheelHand + " " + lowStraight) should be ("Player 2")
+    findWinner(wheelHand + " " + highStraight) should be ("Player 2")
+    findWinner(highStraight + " " + lowStraight) should be ("Player 1")
     //do the same for straight flushes
     val wheelFlush = "AC 2C 3C 4C 5C"
     val lowStraightFlush = "2H 3H 4H 5H 6H"
     val highStraightFlush = "AH KH QH JH TH"
-    assert(findHand(wheelFlush)===StraightFlush(wheelFlush))
-    assert(findWinner(wheelFlush + " " + lowStraightFlush)==="Player 2")
-    assert(findWinner(wheelFlush + " " + highStraightFlush)==="Player 2")
-    assert(findWinner(highStraightFlush + " " + lowStraightFlush)==="Player 1")
+    findHand(wheelFlush)should be (StraightFlush(wheelFlush))
+    findWinner(wheelFlush + " " + lowStraightFlush)should be ("Player 2")
+    findWinner(wheelFlush + " " + highStraightFlush)should be ("Player 2")
+    findWinner(highStraightFlush + " " + lowStraightFlush)should be ("Player 1")
   }
 
-  @Test
-  def straightTiebreaker {
+  "straightTiebreaker" should "break ties between 2 straight hands" in {
     val straightTest1 = "9S TD JC KS QS 3H 2H 4H 5S 6D"
-    assert(findHand("9S TD JC KS QS") === Straight("9S TD JC KS QS"))
-    assert(findWinner(straightTest1) === "Player 1")
+    findHand("9S TD JC KS QS") should be (Straight("9S TD JC KS QS"))
+    findWinner(straightTest1) should be ("Player 1")
   }
 
-  //tests for a flush
-  @Test
-  def flush() {
+  "flush hands" should "work properly" in {
     val flushTest1 = "3H 2H 4H 9H 8H 9S 9D 2C 3S 7S"
-    assert(findHand("3H 2H 4H 9H 8H") === Flush("3H 2H 4H 9H 8H"))
-    assert(findWinner(flushTest1) === "Player 1")
+    findHand("3H 2H 4H 9H 8H") should be (Flush("3H 2H 4H 9H 8H"))
+    findWinner(flushTest1) should be ("Player 1")
   }
 
-  @Test
-  def flushTiebreaker() {
+  "flushTiebreaker" should "break ties between 2 flush hands" in {
     val flushTest1 = "3H 2H 4H 9H 8H 2S 3S 4S 5S 7S"
-    assert(findHand("3H 2H 4H 9H 8H") === Flush("3H 2H 4H 9H 8H"))
-    assert(findHand("2S 3S 4S 5S 7S") === Flush("2S 3S 4S 5S 7S"))
-    assert(findWinner(flushTest1) === "Player 1")
+    findHand("3H 2H 4H 9H 8H") should be (Flush("3H 2H 4H 9H 8H"))
+    findHand("2S 3S 4S 5S 7S") should be (Flush("2S 3S 4S 5S 7S"))
+    findWinner(flushTest1) should be ("Player 1")
   }
 
-  //test for full house
-  @Test
-  def fullHouse() {
+  "fullHouse hands" should "work properly" in {
     val fullHouseTest1 = "2H 2D 4D 4H 4S 9H 9D 2C 3S 7S"
-    assert(findHand("2H 2D 4D 4H 4S") === FullHouse("2H 2D 4D 4H 4S"))
-    assert(findWinner(fullHouseTest1) === "Player 1")
+    findHand("2H 2D 4D 4H 4S") should be (FullHouse("2H 2D 4D 4H 4S"))
+    findWinner(fullHouseTest1) should be ("Player 1")
   }
 
-  @Test
-  def fullHouseTiebreaker() {
+  "fullHouseTiebreaker" should "break ties between 2 full house hands" in {
     val fullHouseTest1 = "JH JD 7D 7H 7S 2H 2D 4D 4H 4S"
-    assert(findHand("JH JD 7D 7H 7S") === FullHouse("JH JD 7D 7H 7S"))
-    assert(findWinner(fullHouseTest1) === "Player 1")
+    findHand("JH JD 7D 7H 7S") should be (FullHouse("JH JD 7D 7H 7S"))
+    findWinner(fullHouseTest1) should be ("Player 1")
     val fullHouseTest2 = "JH JD 7D 7H 7S KH KD 4D 4H 4S"
-    assert(findWinner(fullHouseTest2) === "Player 1")
+    findWinner(fullHouseTest2) should be ("Player 1")
   }
 
-  //test for quads
-  @Test
-  def fourOfAKind() {
+  "fourOfAKind hands" should "work proplerly" in {
     val fourOfAKindTest1 = "2H 2D 2S 2C 4S 9H 9D 3C 3S 7S"
-    assert(findHand("2H 2D 2S 2C 4S") === FourOfAKind("2H 2D 2S 2C 4S"))
-    assert(findWinner(fourOfAKindTest1) === "Player 1")
+    findHand("2H 2D 2S 2C 4S") should be (FourOfAKind("2H 2D 2S 2C 4S"))
+    findWinner(fourOfAKindTest1) should be ("Player 1")
   }
 
-  @Test
-  def fourOfAKindTiebreaker() {
+  "fourOfAKindTiebreaker" should "break ties between 2 four of a kind hands" in {
     val fourOfAKindTest1 = "9H 9D 9C 9S 2S 2H 2D 2S 2C 4S"
-    assert(findHand("9H 9D 9C 9S 2S") === FourOfAKind("9H 9D 9C 9S 2S"))
-    assert(findWinner(fourOfAKindTest1) === "Player 1")
+    findHand("9H 9D 9C 9S 2S") should be (FourOfAKind("9H 9D 9C 9S 2S"))
+    findWinner(fourOfAKindTest1) should be ("Player 1")
   }
 
-  //test for straightflush
-  @Test
-  def straightFlush() {
+  "straightFlush hands" should "work properly" in {
     val straightFlushTest1 = "2H 3H 4H 5H 6H 9H 9D 9C 9S 7S"
-    assert(findHand("2H 3H 4H 5H 6H") === StraightFlush("2H 3H 4H 5H 6H"))
-    assert(findHand("9H 9D 9C 9S 7S") === FourOfAKind("9H 9D 9C 9S 7S"))
-    assert(findWinner(straightFlushTest1) === "Player 1")
+    findHand("2H 3H 4H 5H 6H") should be (StraightFlush("2H 3H 4H 5H 6H"))
+    findHand("9H 9D 9C 9S 7S") should be (FourOfAKind("9H 9D 9C 9S 7S"))
+    findWinner(straightFlushTest1) should be ("Player 1")
 
   }
 
-  @Test
-  def straightFlushTiebreaker() {
+  "straightFlushTiebreaker" should "break ties between 2 straight flush hands" in {
     val straightFlushTest1 = "2H 3H 4H 5H 6H 9H TH JH QH KH"
-    assert(findHand("2H 3H 4H 5H 6H") === StraightFlush("2H 3H 4H 5H 6H"))
-    assert(findHand("9H TH JH QH KH") === StraightFlush("9H TH JH QH KH"))
-    assert(findWinner(straightFlushTest1) === "Player 2")
+    findHand("2H 3H 4H 5H 6H") should be (StraightFlush("2H 3H 4H 5H 6H"))
+    findHand("9H TH JH QH KH") should be (StraightFlush("9H TH JH QH KH"))
+    findWinner(straightFlushTest1) should be ("Player 2")
     val straightFlushTest2 = "9H TH JH QH KH 2H 3H 4H 5H 6H"
-    assert(findWinner(straightFlushTest2) === "Player 1")
+    findWinner(straightFlushTest2) should be ("Player 1")
   }
-*/
 }
