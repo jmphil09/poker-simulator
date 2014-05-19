ce00ae78d3c212e64ec174c84c4b78642e1ee5d3
James Phillips <jmphil09@gmail.com>
2014-05-18 17:23:42 -0700

    the hand class now stores the handValue instead of using a helper function

--- src/main/scala/handManager/Hand.scala
+++ src/main/scala/handManager/Hand.scala
@@ -4,15 +4,34 @@ object Hand {
 }
 
 class Hand(cards: String) {
-  var cardStr = cards
+  val getCards = cards
+  val handValue = 0
 }
 
-case class HighCard(cards: String) extends Hand(cards: String)
-case class OnePair(cards: String) extends Hand(cards: String)
-case class TwoPair(cards: String) extends Hand(cards: String)
-case class ThreeOfAKind(cards: String) extends Hand(cards: String)
-case class Straight(cards: String) extends Hand(cards: String)
-case class Flush(cards: String) extends Hand(cards: String)
-case class FullHouse(cards: String) extends Hand(cards: String)
-case class FourOfAKind(cards: String) extends Hand(cards: String)
-case class StraightFlush(cards: String) extends Hand(cards: String)
+case class HighCard(cards: String) extends Hand(cards: String) {
+	override val handValue = 1
+}
+case class OnePair(cards: String) extends Hand(cards: String) {
+	override val handValue = 2
+}
+case class TwoPair(cards: String) extends Hand(cards: String) {
+	override val handValue = 3
+}
+case class ThreeOfAKind(cards: String) extends Hand(cards: String) {
+	override val handValue = 4
+}
+case class Straight(cards: String) extends Hand(cards: String) {
+	override val handValue = 5
+}
+case class Flush(cards: String) extends Hand(cards: String) {
+	override val handValue = 6
+}
+case class FullHouse(cards: String) extends Hand(cards: String) {
+	override val handValue = 7
+}
+case class FourOfAKind(cards: String) extends Hand(cards: String) {
+	override val handValue = 8
+}
+case class StraightFlush(cards: String) extends Hand(cards: String) {
+	override val handValue = 9
+}
