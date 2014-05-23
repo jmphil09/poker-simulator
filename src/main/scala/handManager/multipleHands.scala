7fb03a5a03386bd8f83189464ca10b823270f0cf
James Phillips <jmphil09@gmail.com>
2014-05-23 15:05:26 -0700

    changed signature of compareMultHands to use sets instead of lists for tied hands

--- src/main/scala/handManager/multipleHands.scala
+++ src/main/scala/handManager/multipleHands.scala
@@ -12,7 +12,7 @@ object multipleHands {
 
   //input: list of hands
   //output: list of a list hands in decreasing order
-  def compareMultHands(handList: List[(Hand, Int)]): List[List[String]] = {
+  def compareMultHands(handList: List[(Hand, Int)]): List[Set[String]] = {
     //this function finds all of the hands which tie "hand" in a list of 3-tuples 
     def findTiesWithHand(hand: Hand, handList: List[(Int, Hand, Int)]): List[(Int, Hand, Int)] = {
       def helper(hand1: Hand, handList1: List[(Int, Hand, Int)], result: List[(Int, Hand, Int)]): List[(Int, Hand, Int)] = {
@@ -25,17 +25,17 @@ object multipleHands {
     val orderedHands = handList.map(x => (x._1.handValue, x._1, x._2)).sortBy(x => x._1).reverse
     
     //this function takes the list of handValues,hands,player numbers and outputs of list of the players in descending order
-    def createStrList(orderedHands: List[(Int, Hand, Int)], result: List[List[String]]): List[List[String]] = {
+    def createStrList(orderedHands: List[(Int, Hand, Int)], result: List[Set[String]]): List[Set[String]] = {
       //this function will subtract the best players from the orderedHands list
       def subtractPlayers(players: List[Int], orderedHands: List[(Int, Hand, Int)]): List[(Int, Hand, Int)] = {
         orderedHands.filterNot(x => players.contains(x._3))
       }
       //takes a list of ints and creates a list of players with those ints
-      def genList(listNums: List[Int], players: List[String]): List[String] = {
+      def genList(listNums: List[Int], players: Set[String]): Set[String] = {
         if (listNums.isEmpty) players
         else {
           val playerStr = "Player " ++ listNums.head.toString
-          genList(listNums.tail, players ++ List(playerStr))
+          genList(listNums.tail, players ++ Set(playerStr))
         }
       }
 
@@ -43,7 +43,7 @@ object multipleHands {
       else {
         //ints of the best players
         val bestPlayers = findTiesWithHand(orderedHands.head._2, orderedHands).map(x => x._3)
-        createStrList(subtractPlayers(bestPlayers, orderedHands), result ++ List(genList(bestPlayers, List())))
+        createStrList(subtractPlayers(bestPlayers, orderedHands), result ++ List(genList(bestPlayers, Set())))
       }
     }
     createStrList(orderedHands, List())
