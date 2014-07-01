d9b2a02a7831d532f903667a6d1020b9904b377e
James Phillips <jmphil09@gmail.com>
2014-07-01 14:07:30 -0700

    updated hw.scala

--- hw.scala
+++ hw.scala
@@ -1,1 +1,6 @@
-object pokerSimulatorGreeting { def main(args: Array[String]) = println("Welcome to poker-simulator!") }
+object pokerSimulatorGreeting { 
+  def main(args: Array[String]) = {
+    println("Welcome to poker-simulator!")
+    println("Play needs to be fixed before you can run this program!")
+  }
+}
